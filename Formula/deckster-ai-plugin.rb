8e07545afd94bed1d83fa2b0f02d690ac2832b1e
George S <georgestribley@outlook.com>
2025-11-17 06:45:13 -0500

    Initial brew tap commit for deckster-ai-plugin

--- Formula/deckster-ai-plugin.rb
+++ Formula/deckster-ai-plugin.rb
@@ -2,7 +2,7 @@ class DecksterAiPlugin < Formula
   desc "Deckster AI Plugin Update"
   homepage "https://github.com/bcg-updates/"
   url "https://github.com/bcg-updates/homebrew-deckster-ai/releases/download/v2.1/deckster-ai-plugin-2.1.tar.gz"
-  sha256 "cf26b010ac648aedcbad7dc96d5039a660ea72cc43146708d39aec015367b418"
+  sha256 "49e9a89a7fd26b31f9a50a6089e0a69d12ef304587f4bab4418db1c815b97697"
   version "2.1"
   depends_on "python@3.12"
 
@@ -18,7 +18,7 @@ class DecksterAiPlugin < Formula
     <<~EOS
     To finish installation, add the following line to .zshrc or .bashrc  
   source #{libexec}/completion.sh
-Then reopen your terminal app. You're updated Deckster AI plugin is now ready to use.
+Then reopen your terminal app. Your updated Deckster AI plugin is now ready to use.
     EOS
   end
 end
